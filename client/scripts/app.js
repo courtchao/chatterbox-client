9e4f9ac20af26ee857d959b159e23ab929a94644
courtchao <couchao@ucdavis.edu>
2018-06-22 19:47:04 -0700

    Successfully rendered rooms and fetched most recent messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,93 @@
-// YOUR CODE HERE:
+var app = {
+
+  server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+
+  currRoomName: '4chan',
+
+  init: function (data) {
+  //**************************
+  //**************************
+  },
+
+  send: function(message) {
+    $.ajax({
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+
+  },
+
+  fetch: function() {
+    $.ajax({
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages?order=-createdAt',
+      type: 'GET',
+      // data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log(data);
+        var messageList = data.results;
+        var filteredList = messageList.filter(function(item) {
+          //screen for attacks
+          return item.roomname === app.currRoomName; 
+        })
+        for (var obj of filteredList) {
+          app.renderMessage(obj);
+        }
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });  
+  },
+
+  renderMessage: function(message) {
+    var element = $('<div class=\'messages\'></div>');
+    $(element).text(`${message.username}: ${message.text}`);
+    $('#chats').prepend(element);
+  },
+
+// 
+  clearMessages: function(){
+    $('#chats').empty();
+  },
+
+  renderRoom: function(roomName) {
+    //when "New room..." in selector is chosen be able to add a new room to dropdown list
+    // 
+    var room = $(`<option value=${roomName}></option>`)
+    $(room).text(`${roomName}`);
+    $('#roomSelect').append(room);
+  }
+};
+
+$(document).ready(function() {
+  $('#roomSelect').on('change', function() {
+    app.clearMessages();
+    app.currRoomName = $('#roomSelect').val();
+    app.fetch();
+  })
+});
+
+// $(document).ready(function() {
+
+//   $('.messages').on('click', function () {
+//     $(`.${message.username}`).toggleClass('.friend');
+//   })  
+
+//   $('#roomSelect').change(function() {
+
+// // })
+// });
+//})
+  //toggleClass friend
+
+
+
